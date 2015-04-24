5f3ebf2389836265dfbf7705229add5cf977a6e1
Adrien Cadet <acadet@live.fr>
2015-04-24 14:32:34 -0700

    Scaffolds DAO

---
+++ rothrock/IPreferenceDAO.swift
@@ -0,0 +1,19 @@
+//
+//  IPreferenceDAO.swift
+//  rothrock
+//
+//  Created by Adrien on 24/04/15.
+//  Copyright (c) 2015 RailsSchool. All rights reserved.
+//
+
+import Foundation
+
+public protocol IPreferenceDAO {
+    func getTwoHourNotificationPreference() -> TwoHourNotificationPreference
+    
+    func setTwoHourNotificationPreference(value: TwoHourNotificationPreference)
+    
+    func getDayNotificationPreference() -> DayNotificationPreference
+    
+    func setDayNotificationPreference(value: DayNotificationPreference)
+}
---
+++ rothrock/IUserDAO.swift
@@ -0,0 +1,27 @@
+//
+//  IUserDAO.swift
+//  rothrock
+//
+//  Created by Adrien on 24/04/15.
+//  Copyright (c) 2015 RailsSchool. All rights reserved.
+//
+
+import Foundation
+
+public protocol IUserDAO {
+    func exists(id: Int) -> Bool
+    
+    func find(id: Int) -> User
+    
+    func save(user: User)
+    
+    func getCurrentUsername() -> String
+    
+    func setCurrentUsername(value: String)
+    
+    func getCurrentUserToken() -> String
+    
+    func setCurrentUserToken(value: String)
+    
+    func hasCurrentUser() -> Bool
+}
