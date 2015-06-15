b9b5983281938195dc9bce98b8b5c01452ca76f0
Adrien Cadet <acadet@live.fr>
2015-06-15 11:52:18 -0700

    Cleans Swift project before starting new version of class list

---
+++ rothrock/BaseController.swift
@@ -0,0 +1,25 @@
+//
+//  BaseController.swift
+//  rothrock
+//
+//  Created by Adrien on 15/06/15.
+//  Copyright (c) 2015 RailsSchool. All rights reserved.
+//
+
+import Foundation
+import SwiftEventBus
+
+public class BaseController {
+    public func fork() {
+        SwiftEventBus.post(ProgressForkEvent.NAME)
+    }
+    
+    public func done() {
+        SwiftEventBus.post(ProgressDoneEvent.NAME)
+    }
+    
+    public func publishError(message: String) {
+        SwiftEventBus.post(ErrorEvent.NAME, sender: ErrorEvent(message: message))
+        done()
+    }
+}
---
+++ rothrock/controllers/ClassListController.swift
@@ -0,0 +1,13 @@
+//
+//  ClassListController.swift
+//  rothrock
+//
+//  Created by Adrien on 15/06/15.
+//  Copyright (c) 2015 RailsSchool. All rights reserved.
+//
+
+import Foundation
+
+internal class ClassListController: BaseController {
+    
+}
